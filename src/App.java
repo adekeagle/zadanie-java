97b5a61cb11672ad4299e7e7fe8f80ec51ab28e4
adekdevcode <adekdevcode@gmail.com>
2022-06-01 18:47:37 +0200

    Added solution

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/*
+    Napisz program, w którym dla przykładowej tablicy liczb całkowitych
+    obliczysz sumę tych elementów, które znajdują się pod parzystym indeksem
+ */
+
+public class App {
+    public static void main(String[] args) {
+        int[] NumberTable = new int[]{ 2, 5, 3, 4, 1, 10 };
+        System.out.println(sumOfEvenElements(NumberTable));
+    }
+
+    private static int sumOfEvenElements(int[] tab) {
+        int sum = Arrays.stream(tab).filter(i -> i % 2 == 0).sum();
+        return sum;
+    }
+}
